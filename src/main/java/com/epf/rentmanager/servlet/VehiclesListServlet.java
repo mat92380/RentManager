25573ce2f0dd565adeca3e6290052517139dd932
Matthieu <matthieu.jehanne@epfedu.fr>
2023-03-03 10:06:02 +0100

    ajout de la liste de vehicule

--- src/main/java/com/epf/rentmanager/servlet/VehiclesListServlet.java
+++ src/main/java/com/epf/rentmanager/servlet/VehiclesListServlet.java
@@ -1,5 +1,8 @@
 package com.epf.rentmanager.servlet;
 
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.service.VehicleService;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,13 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //une servlet par url
+
 @WebServlet("/cars")//Quand on va sur /home ca envoie vers la homepage grace à la servlet
 public class VehiclesListServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private VehicleService vehicleService = VehicleService.getInstance();
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try{
 
+            request.setAttribute("vehicles", vehicleService.findAll());
+
+
+        }catch (ServiceException e ){
+            e.printStackTrace();
+        }
         this.getServletContext().getRequestDispatcher("/WEB-INF/views/vehicles/list.jsp").forward(request, response);
+
 
         //permets de dire qu est ce qu on va envoyer vers la home servlet (homepage)
     }
